ef95711f1b5799c70c7cb4fd8d50bc41089f2b90
emmilydev <alextavriayt@gmail.com>
2022-08-11 16:26:45 -0300

    refactor(deps): include the commons-validation dependency with the api configuration instead of implementation

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api("org.jetbrains:annotations:22.0.0")
-    implementation("team.unnamed:commons-validation:3.1.0")
+    api("team.unnamed:commons-validation:3.1.0")
 
     compileOnly("org.spigotmc:spigot:1.8.8-R0.1-SNAPSHOT")
 }
