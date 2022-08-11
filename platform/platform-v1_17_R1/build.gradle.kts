c0e8846c7c98abdb5d63bbe5c6a4b1e1fa9b12dc
emmilydev <alextavriayt@gmail.com>
2022-08-11 16:23:02 -0300

    build(1.17): set the JDK version to 16 using toolchains

--- platform/platform-v1_17_R1/build.gradle.kts
+++ platform/platform-v1_17_R1/build.gradle.kts
@@ -2,3 +2,9 @@ dependencies {
     api(project(":scoreboard-api"))
     compileOnly("org.spigotmc:spigot:1.17.1-R0.1-SNAPSHOT")
 }
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(16))
+    }
+}
